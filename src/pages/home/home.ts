856f77a810f7c9de3ba5932c4a9e584f31b32899
erickkf600 <erickkf600@gmail.com>
2018-10-05 11:44:07 -0300

    Projeto Ionic Lazy Loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import { IonicApp } from 'ionic-angular/components/app/app-root';
+
+@IonicPage()
 
 @Component({
   selector: 'page-home',
@@ -9,6 +12,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  login(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
+  cadastro(){
+    this.navCtrl.push('CategoriasPage');
   }
 
 }
